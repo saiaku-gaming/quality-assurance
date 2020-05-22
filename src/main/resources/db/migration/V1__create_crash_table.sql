c3577e94b6179c65171d6a2efafa9c4944bd8a83
Mikael Berglund <mail@mikael-berglund.se>
2020-05-22 19:07:08 +0200

    Basic crash list in place

--- src/main/resources/db/migration/V1__create_crash_table.sql
+++ src/main/resources/db/migration/V1__create_crash_table.sql
@@ -3,12 +3,12 @@ create table crashes
     id serial
         constraint crashes_pk
             primary key,
-    path_on_disc text not null,
+    path_on_disc varchar not null,
     ts timestamp default now() not null,
-    crash_in_version text,
+    crash_in_version varchar,
     fixed_in_version int,
-    user_description text,
-    fixer_description text
+    user_description varchar,
+    fixer_description varchar
 );
 
 comment on table crashes is 'metadata about crashes, the actual data is on disk';

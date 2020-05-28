db4bebee1d9d1f3802989b673b735502a5016f7f
Mikael Berglund <mail@mikael-berglund.se>
2020-05-28 21:31:03 +0200

    Trying to understand error while booting

--- src/main/kotlin/com/valhallagame/qa/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/valhallagame/qa/configuration/SecurityConfiguration.kt
@@ -3,29 +3,33 @@ package com.valhallagame.qa.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.util.matcher.RequestMatcher
+import javax.servlet.http.HttpServletRequest
 
 
 @Configuration
 @Profile("prod")
-class SecurityConfigProd : WebSecurityConfigurerAdapter() {
+@Order(2)
+class OauthConfig : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http
+
+        http.requestMatcher(BasicRequestMatcher())
                 .authorizeRequests()
-                .antMatchers("/public/**")
-                .permitAll()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**")
-                .hasRole("JENKINS")
+                .antMatchers("/**").authenticated()
                 .and()
                 .httpBasic()
+
+        http.authorizeRequests()
+                .antMatchers("/public/**")
+                .permitAll()
                 .and()
                 .authorizeRequests()
                 .anyRequest()
@@ -45,7 +49,17 @@ class SecurityConfigProd : WebSecurityConfigurerAdapter() {
                 .build()
         return InMemoryUserDetailsManager(user)
     }
+
+    private class BasicRequestMatcher : RequestMatcher {
+        override fun matches(request: HttpServletRequest): Boolean {
+            val auth = request.getHeader("Authorization")
+            return auth != null && auth.startsWith("Basic")
+        }
+    }
+
+
 }
+
 
 @Configuration
 @Profile("dev")
